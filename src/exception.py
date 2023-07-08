1f25815a17988997769fad77cec718c424175556
kumar <sachin.u.kumar@capgemini.com>
2023-07-08 20:48:14 +0530

    model adding and regression

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -20,3 +20,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+
+if __name__ == "__main__":
+    try:
+        a = 10/0
+    except Exception as e:
+        logging.info("exception got occured")
+        raise CustomException(e,sys)
